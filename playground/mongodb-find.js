a2c8631b048aa00da831455225e424f40ebcd0d8
Pankaj K S <SPankaj@ESTUATE.COM>
2018-02-12 10:50:47 +0530

    The commit includes the count method usage and the find method usage

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -1,6 +1,6 @@
 const {MongoClient, ObjectId} = require('mongodb');
 
-const url = 'mongodb://localhost:27017/Todos'
+const url = 'mongodb://localhost:27017/'
 
 MongoClient.connect(url, (err, db) => {
 	if(err) {
@@ -8,19 +8,29 @@ MongoClient.connect(url, (err, db) => {
 		process.exit(0);
 	}
 
-	console.log(' =============== Connected to the MongoDB sevrer ================== ');
+	console.log(' =============== Connected to the MongoDB sevrer ================== \n\n');
 
-	var myDb = db.db('Todos'); 
-	myDb.collection('Todos').find({
-		_id: new ObjectId('5a78360b20b39407f46ffec6')
-	}).toArray().then((docs) => {
-	console.log(JSON.stringify(docs, undefined, 2));
-	console.log(' ===== Disconnected from the MongoDB sevrer Successfully ========== ');
+	var myDb = db.db('Todos').collection('Todos'); 
+	// myDb.collection('Todos').find({
+	// 	_id: new ObjectId('5a78360b20b39407f46ffec6')
+	// }).toArray().then((docs) => {
+	// console.log(JSON.stringify(docs, undefined, 2));
+	// console.log(' ===== Disconnected from the MongoDB sevrer Successfully ========== ');
+	// }, (err) => {
+	// console.log('Unable to fetch the data');
+	// console.log(' =============== Error while fetching data. Connection Aborted !!! ================== ');
+	// });
+	myDb.find({completed: false}).count().then((count) => {
+		console.log(`The number of records is : ${count}`);
+		if(count !== 0){
+			myDb.find({completed: false}).toArray().then((docs) => {
+				console.log(JSON.stringify(docs, undefined, 2));
+			});
+		}
 	}, (err) => {
-	console.log('Unable to fetch the data');
-	console.log(' =============== Error while fetching data. Connection Aborted !!! ================== ');
+		console.log('Unable to fetch the records .... !!!\n', err);
 	});
 
-	//db.close();	
+	// db.close();	
 
 });
